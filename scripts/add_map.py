c8a874142b81f3e36259ddfecc27c6c35073c42f
5730289021-NN <norawit.n@mail.kmutt.ac.th>
2023-02-13 22:21:26 +0700

    2to3

--- scripts/add_map.py
+++ scripts/add_map.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 PKG = 'map_store'
 import roslib; roslib.load_manifest(PKG)
@@ -6,40 +6,40 @@ import rospy
 import sys
 import map_store.srv
 
-save_as = None
+map_name = None
 try:
-    save_as = sys.argv[1]
+    map_name = sys.argv[1]
 except:
-    print "You must specify a name for the map on the server"
+    print("You must specify a name for the map on the server")
     sys.exit(1)
 
-print "Waiting for /dynamic_map..."
-rospy.wait_for_service('/dynamic_map')
-print "Waiting for /save_map..."
+# print("Waiting for /dynamic_map...")
+# rospy.wait_for_service('/dynamic_map')
+print("Waiting for /save_map...")
 rospy.wait_for_service('/save_map')
-print "Waiting for /list_maps..."
+print("Waiting for /list_maps...")
 rospy.wait_for_service('/list_maps')
-print "Waiting for /delete_map..."
+print("Waiting for /delete_map...")
 rospy.wait_for_service('/delete_map')
 
-print "Checking for duplicates..."
+print("Checking for duplicates...")
 list_last_maps = rospy.ServiceProxy('/list_maps', map_store.srv.ListMaps)
 delete_map = rospy.ServiceProxy('/delete_map', map_store.srv.DeleteMap)
 maps = []
 try:
     maps = list_last_maps().map_list
 except:
-    print "Getting maps from the map_manager has failed"
+    print("Getting maps from the map_manager has failed")
     sys.exit(2)
 
 for i in maps:
-    if (i.name == save_as):
-        print "Deleting map", i.map_id
+    if (i.name == map_name):
+        print("Deleting map", i.map_id)
         delete_map(i.map_id)
 
-print "Starting service..."
+print("Starting service...")
 save_map = rospy.ServiceProxy('/save_map', map_store.srv.SaveMap)
-print "Saving map as", save_as
-save_map(save_as)
-print "Done"
+print("Saving map as", map_name)
+save_map(map_name)
+print("Done")
 
